d5bacd42e3543aa2c32ec0e0c00a9b95b8df835a
sarafchetan <saraf.chetan@gmail.com>
2020-02-07 01:22:11 +0530

    Added some more Junit test cases

--- com-mathmaticalexamp-set/src/test/java/SetTest.java
+++ com-mathmaticalexamp-set/src/test/java/SetTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SetTest {
@@ -14,33 +15,49 @@ public class SetTest {
 
 
 
-
+	@Ignore
 	@Test 
 	public void checkEmptySet() throws Exception { 
 
 		assertNotNull(new TestClass().isempty(numset));
 
 	}
-
+	
+	@Ignore
 	@Test
 	public void checkUniqueSet() throws Exception {
 		int[] expectedArray=new int[] {1,2,3,5,6,4,8,7,-1,-2,100,-1};
 
-
-		assertArrayEquals(expectedArray,new TestClass().unique(numset));
+		assertArrayEquals(expectedArray, new TestClass().unique(numset));
 
 	}
+	
+	
 
-
+	@Ignore
 	@Test
 	public void checkCompareSet() throws Exception {
 		Assert.assertEquals(true, new TestClass().compare(numsetA, numsetB));
 	}
 
+	@Ignore
 	@Test
 	public void setSubset() throws Exception{
 		boolean result=new TestClass().issetSubset(numsetA, numsetB,numsetA.length, numsetB.length);
 		Assert.assertEquals(true, result);
 
+	}
+	
+	@Ignore
+	@Test
+	public void checkDeleteSet() {
+		
+		Assert.assertEquals(false, new TestClass().deleteSet(numsetA));
+	}
+	
+	@Test
+	public void findSet() {
+		
+		Assert.assertEquals(true,new TestClass().findSet(numsetB));
 	}
 }
